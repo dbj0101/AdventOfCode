3d87093e0b73279eecd6094dbff9ceb354a0016e
DB <100801952+dbj0101@users.noreply.github.com>
2024-12-02 20:58:27 -0500

    Add files via upload

--- 2024/Day2/Puzzle2nd.js
+++ 2024/Day2/Puzzle2nd.js
@@ -1,5 +1,6 @@
 let data = __dirname + "/testdata.txt";
-//data = __dirname + "/data.txt";
+//
+data = __dirname + "/data.txt";
 
 let fs = require("fs");
 inStr = fs.readFileSync(data).toString("utf-8");
@@ -9,17 +10,13 @@ let inData = inStr.split("\n");
 
 //inData = [inData[0], inData[1]];
 
-let total = 0;
-const calcSafe = inData.map((val) => {
-  //console.log("val", val);
-  items = val.split(" ");
-  //console.log("items:", items);
-  if (items.length < 1 || items[0] == items[1]) return 0;
+const checkSuccess = (items) => {
+  if (items.length < 1 || items[0] == items[1]) return false;
   else if (Number(items[0]) < Number(items[1])) {
     //row is ascending
     //console.log("row is ascending- item.length:", items.length, items);
     for (let i = 0; i < items.length; i++) {
-      if (i == items.length - 1) total++;
+      if (i == items.length - 1) return true;
       else if (
         Number(items[i]) < Number(items[i + 1]) &&
         Math.abs(Number(items[i]) - Number(items[i + 1])) <= 3
@@ -32,7 +29,7 @@ const calcSafe = inData.map((val) => {
     // console.log("row is decending - item.length:", items.length, items);
     for (let i = 0; i < items.length; i++) {
       //console.log("decending compare i:", i, items[i], items[i + 1]);
-      if (i == items.length - 1) total++;
+      if (i == items.length - 1) return true;
       else if (
         Number(items[i]) > Number(items[i + 1]) &&
         Math.abs(Number(items[i]) - Number(items[i + 1])) <= 3
@@ -40,6 +37,28 @@ const calcSafe = inData.map((val) => {
         //console.log();
         continue;
       } else break;
+    }
+  }
+};
+
+let total = 0;
+const calcSafe = inData.map((val) => {
+  //console.log("val", val);
+  items = val.split(" ");
+  //console.log("items:", items);
+  if (checkSuccess(items)) total++;
+  else {
+    //console.log("items:", items);
+    for (let i = 0; i < items.length; i++) {
+      let spliceItems = items.map((val, idx) => {
+        if (idx != i) return val;
+      });
+      spliceItems.splice(i, 1);
+      //console.log("spliceItems:", spliceItems);
+      if (checkSuccess(spliceItems)) {
+        total++;
+        break;
+      }
     }
   }
 });
