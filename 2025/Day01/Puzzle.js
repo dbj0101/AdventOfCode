21b9fa7845c8a2387efc1b6b037167ca6116daab
David Brinton <dbj0101@gmail.com>
2025-12-03 20:19:35 -0500

    Day1 complete

--- 2025/Day01/Puzzle.js
+++ 2025/Day01/Puzzle.js
@@ -1,10 +1,52 @@
 let data = __dirname + "/testdata.txt";
-//data = __dirname + "/data.txt";
+//
+data = __dirname + "/data.txt";
 
 let fs = require("fs");
 inStr = fs.readFileSync(data).toString("utf-8");
 
-//add code here:
-
 let inData = inStr.split("\n");
-console.log(inData);
+
+const padLock = {
+    currentLoc: 50,
+    min: 0,
+    max: 99
+}
+
+const moveDir = (pl, dir, num) => {
+    while(num > pl.max){
+        num = num - (pl.max + 1);
+    }
+    //console.log(pl.currentLoc, dir, num);
+    if(dir=="L") {
+        if(num > pl.currentLoc){
+            pl.currentLoc = pl.max - num + pl.currentLoc + 1;
+        }
+        else
+            pl.currentLoc = pl.currentLoc - num;
+    }
+    else if(dir=="R") {
+        if((pl.currentLoc + num)>pl.max) {
+            pl.currentLoc = (pl.max - pl.currentLoc - num + 1) * (-1);
+        }
+        else
+            pl.currentLoc = pl.currentLoc + num;
+    }
+    //console.log(pl.currentLoc);
+}
+
+const checkMove = (pl, moveStr) => {
+    const dir = moveStr.slice(0,1)
+    let num = Number(moveStr.slice(1));
+    moveDir(pl, dir, num);
+}
+
+let count=0;
+//console.log("Starting Location", padLock.currentLoc);
+inData.forEach((val) => {
+    checkMove(padLock, val)
+    if(padLock.currentLoc==0 || padLock.currentLoc==-0)
+        count = count + 1;
+    console.log("Current Location", padLock.currentLoc, "Movement", val, "Count",count);
+});
+console.log("Total Count",count);
--- 2025/Day01/Puzzle2nd.js
+++ 2025/Day01/Puzzle2nd.js
@@ -1,8 +1,68 @@
 let data = __dirname + "/testdata.txt";
-//data = __dirname + "/data.txt";
+//
+data = __dirname + "/data.txt";
 
 let fs = require("fs");
 inStr = fs.readFileSync(data).toString("utf-8");
 
 let inData = inStr.split("\n");
-console.log(inData);
+
+const padLock = {
+    currentLoc: 50,
+    min: 0,
+    max: 99
+}
+
+const moveDir = (pl, dir, num) => {
+    let zeroCount=0;
+    while(num > pl.max){
+        num = num - (pl.max + 1);
+        zeroCount = zeroCount + 1;
+    }
+    //console.log(pl.currentLoc, dir, num);
+    if(dir=="L") {
+        if(num > pl.currentLoc){
+            if(pl.currentLoc>0)
+                zeroCount = zeroCount + 1;
+            pl.currentLoc = pl.max - num + pl.currentLoc + 1;
+        }
+        else{
+            pl.currentLoc = pl.currentLoc - num;
+            if((pl.currentLoc==0 || pl.currentLoc==-0) && num>0){
+                zeroCount = zeroCount + 1;
+                pl.currentLoc=0;
+            }
+        }
+    }
+    else if(dir=="R") {
+        if((pl.currentLoc + num)>pl.max) {
+            pl.currentLoc = (pl.max - pl.currentLoc - num + 1) * (-1);
+            zeroCount = zeroCount + 1;
+        }
+        else{
+            pl.currentLoc = pl.currentLoc + num;
+            // if((pl.currentLoc==0 || pl.currentLoc==-0) && num>0){
+            //     zeroCount = zeroCount + 1;
+            //     pl.currentLoc=0;
+            // }
+        }
+    }
+    //console.log(pl.currentLoc);
+    return zeroCount;
+}
+
+const checkMove = (pl, moveStr) => {
+    const dir = moveStr.slice(0,1)
+    let num = Number(moveStr.slice(1));
+    return moveDir(pl, dir, num);
+}
+
+let count=0;
+//console.log("Starting Location", padLock.currentLoc);
+inData.forEach((val) => {
+    let inc = checkMove(padLock, val)
+    count = count + inc;
+    console.log("New Location", padLock.currentLoc, "Movement", val, "Count",count, "Pass Zero",inc);
+});
+console.log("Total Count",count);
+//6612 is too low
